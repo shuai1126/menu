5294ed92e2c100a2fbe192644a891385a2ad692c
“shuai1126” <1042820762@qq.com>
2021-01-30 16:39:06 +0800

    详情

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Process from './Process'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
