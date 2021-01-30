8fdd2939a2159a893ba898aa04ad5e2fc3946cba
kinzzn <jin2016.illian@gmail.com>
2021-01-30 21:56:17 +0800

    user page and bottom-banner using material-ui

---
+++ src/pages/user/index.js
@@ -0,0 +1,70 @@
+import React,{Component, Fragment} from 'react'
+import './style.css'
+import AccountIcon from '@material-ui/icons/AccountCircleRounded'
+import StarIcon from '@material-ui/icons/StarOutlineOutlined'
+import ClassIcon from '@material-ui/icons/ClassOutlined'
+import CommentIcon from '@material-ui/icons/CommentOutlined'
+import ShareIcon from '@material-ui/icons/ShareOutlined'
+import SettingIcon from '@material-ui/icons/SettingsOutlined'
+class TodoApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <div id="title">开启美食之旅<span class="title-logo"><AccountIcon /></span></div>
+          <div id="setlist">
+            <ul>
+              <li>我的收藏<span class="logo"><StarIcon /></span> </li><hr/>
+              <li>我的课程<span class="logo"><ClassIcon /> </span></li><hr/>
+              <li>意见反馈<span class="logo"><CommentIcon /> </span></li><hr/>
+              <li>分享给朋友<span class="logo"><ShareIcon /> </span></li><hr/>
+              <li>设置<span class="logo"><SettingIcon /> </span></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+
+
+class TodoList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+
+export default TodoApp;
